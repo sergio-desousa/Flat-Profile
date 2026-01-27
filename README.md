6d3143a58c8b7f6778f2ba10027f304c07a336d9
Sergio de Sousa <sergio@serso.com>
2026-01-27 16:11:21 -0500

    Initial skeleton for Flat::Profile

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# NAME
+
+Flat::Profile - It's new $module
+
+# SYNOPSIS
+
+    use Flat::Profile;
+
+# DESCRIPTION
+
+Flat::Profile is ...
+
+# LICENSE
+
+Copyright (C) Sergio de Sousa.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Sergio de Sousa <sergio@serso.com>
---
+++ lib/Flat/Profile.pm
@@ -0,0 +1,39 @@
+package Flat::Profile;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Flat::Profile - It's new $module
+
+=head1 SYNOPSIS
+
+    use Flat::Profile;
+
+=head1 DESCRIPTION
+
+Flat::Profile is ...
+
+=head1 LICENSE
+
+Copyright (C) Sergio de Sousa.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Sergio de Sousa E<lt>sergio@serso.comE<gt>
+
+=cut
+
